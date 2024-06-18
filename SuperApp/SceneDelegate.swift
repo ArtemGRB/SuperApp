9f544a04c1e316fe45c7172114d0e6d2003ee658
Артем Гребнев <artemgrebnev@iMac-Artem.local>
2024-06-18 19:26:17 +0700

    убрал сториборд

--- SuperApp/SceneDelegate.swift
+++ SuperApp/SceneDelegate.swift
@@ -13,10 +13,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let libraryCollection = ViewController()
+        window?.rootViewController = libraryCollection
+        window?.makeKeyAndVisible()
 
-        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
